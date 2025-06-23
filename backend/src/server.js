78bdbc52c6bedf99ec474eea96ef9b2400500c60
MIhirDas10 <dasmihir2911@gmail.com>
2025-06-24 01:54:22 +0600

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 import notesRoutes from "../src/routes/notesRoutes.js"
 import { connectDB } from "./config/db.js"
@@ -10,16 +11,29 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 // --- middleware ---
 // it will parse JSON bodies: req.body
-app.use(cors({
-    origin:"http://localhost:5173",
-}))
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin:"http://localhost:5173",
+    }))
+}
+
+
 app.use(express.json())  // just because we added this middleware, we can get things from the body (title, content fields) written in notesController.js
 app.use(rateLimiter)
 
 app.use("/api/notes", notesRoutes)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+})
+}
 
 // simple custom middleware
 // app.use((req, res, next) => {
